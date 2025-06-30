0de0cef6d73ede6e3f5f998a1f1e00170293e8e8
ffsant <ffsant@senado.leg.br>
2025-06-30 22:20:48 +0000

    primeira versão da API

---
+++ code-run-tracker-api/src/main/java/br/edu/idp/cc/coderuntracker/controller/ExecutionController.java
@@ -0,0 +1,50 @@
+package br.edu.idp.cc.coderuntracker.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.idp.cc.coderuntracker.model.Execution;
+import br.edu.idp.cc.coderuntracker.repository.ExecutionRepository;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/executions")
+public class ExecutionController {
+
+    @Autowired
+    private ExecutionRepository executionRepository;
+
+    @PostMapping
+    public ResponseEntity<?> registerExecution(@RequestBody Execution request, HttpServletRequest httpRequest) {
+
+        try {
+            Execution exec = new Execution();
+
+            exec.setInstitution(request.getInstitution());
+            exec.setCourse(request.getCourse());
+            exec.setSubject(request.getSubject());
+            exec.setSemester(request.getSemester());
+            exec.setStudentId(request.getStudentId());
+            exec.setTaskCode(request.getTaskCode());
+            exec.setLocalUser(request.getLocalUser());
+            exec.setExecutedAt(request.getExecutedAt() != null ? request.getExecutedAt() : LocalDateTime.now());
+            exec.setIpAddress(httpRequest.getRemoteAddr());
+        
+            executionRepository.save(exec);            
+
+            return ResponseEntity.ok("Execution successfully registered.");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error: " + e.getMessage());
+        }
+
+        
+
+    }
+}
---
+++ code-run-tracker-api/src/test/java/br/edu/idp/cc/coderuntracker/CodeRunTrackerApiApplicationTests.java
@@ -0,0 +1,15 @@
+package br.edu.idp.cc.coderuntracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRunTrackerApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
+
+
